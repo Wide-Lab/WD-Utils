20168d529855288ed78d2fcd306560861dd15016
Widelab - Leonardo <leonardo.noro@widelab.com.br>
2025-02-13 09:23:20 -0300

    fix/reseted project

--- index.ts
+++
@@ -1,7 +0,0 @@
-import * as Dates from './src/Dates';
-import * as Formatters from './src/Formatters';
-import * as Numbers from './src/Numbers';
-import * as Strings from './src/Strings';
-import * as Validators from './src/Validators';
-
-export { Dates, Formatters, Numbers, Strings, Validators };
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as Dates from './Dates';
+import * as Formatters from './Formatters';
+import * as Numbers from './Numbers';
+import * as Strings from './Strings';
+import * as Validators from './Validators';
+
+export { Dates, Formatters, Numbers, Strings, Validators };
