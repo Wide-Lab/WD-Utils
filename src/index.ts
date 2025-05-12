07eeb4b331639d3fffef13cdd137d41d8eda47d5
Widelab - Leonardo <leonardo.noro@widelab.com.br>
2025-05-12 09:55:34 -0300

    fix: update package version to 1.0.10; import Files module and export it

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as Colors from './Colors';
 import * as Dates from './Dates';
+import * as Files from './Files';
 import * as Formatters from './Formatters';
 import * as Numbers from './Numbers';
 import * as Strings from './Strings';
@@ -9,9 +10,9 @@ export { Colors, Dates, Formatters, Numbers, Strings, Validators };
 
 export {
   Colors as WDColors,
-  Dates as WDDates,
-  Formatters as WDFormatters,
+  Dates as WDDates, Files as WDFiles, Formatters as WDFormatters,
   Numbers as WDNumbers,
   Strings as WDStrings,
-  Validators as WDValidators,
+  Validators as WDValidators
 };
+
